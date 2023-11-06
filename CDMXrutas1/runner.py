2df99e377acb2f68fb25dc83f31995255aaf557a
PabloBenitezR <141075173+PabloBenitezR@users.noreply.github.com>
2023-11-05 22:45:09 -0600

    Add files via upload

--- CDMXrutas1/runner.py
+++ CDMXrutas1/runner.py
@@ -34,6 +34,7 @@ else:
 
 from sumolib import checkBinary 
 import traci
+import traci.constants as tc  # noqa
 import math
 import random
 import re
@@ -43,10 +44,12 @@ bt=60.0
 et=3600.0
 
 #edges closed
-edgesclosed="616921713#6 618028623#12 79431129 -124181617#1 -124181617#3 -124181617#2 846008895"
+edgesclosed="616921713#6 616921713#7 616921713#9 616921713#10 616921713#11 616921713#12 616921713#13 616921713#14 616921713#15 1135302411#0 618028624#1 421567701#0 421567701#1 24850149#0 24850149#1 24850149#2 24850149#3 24850149#4 616927361"
 
 #vehicles disallowed
 vehdisa="private"
+
+    
 
 def closingedges():
     ar=edgesclosed.split(' ')
@@ -80,6 +83,11 @@ def createejbloqueo():
     h.write("<?xml version='1.0' encoding='UTF-8'?>\n<configuration xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:noNamespaceSchemaLocation='http://sumo.dlr.de/xsd/sumoConfiguration.xsd'>\n<input>\n<net-file value='osm.net.xml.gz'/>\n<route-files value='ejbloqueo.rou.xml'/>\n<additional-files value='closededges.add.xml'/>\n</input>\n</configuration>")
     h.close()
 
+    #agregamos una lista de las calles cerradas para la manifestacion
+    arreglolanes=["616921713#6","616921713#7","616921713#9","616921713#10","616921713#11","616921713#12","616921713#13","616921713#14","616921713#15","1135302411#0","618028624#1","421567701#0","421567701#1","24850149#0","24850149#1","24850149#2","24850149#3","24850149#4"]
+
+     
+
 def run():
     step = 0
     #elegimos una semilla aleatoria
@@ -87,11 +95,23 @@ def run():
     while traci.simulation.getMinExpectedNumber() > 0:
         traci.simulationStep()    
         step += 1
+    primer=arreglolanes[0];
+    ultima=arreglolanes[-1];
+    i=0;
+    while(i<1000):
+        traci.person.add('person'+str(i),edgeID=primer,pos=10, depart=i, typeID='DEFAULT_PEDTYPE')
+        traci.person.appendWalkingStage('person'+str(i), arreglolanes, 10, duration=-1, speed=1, stopID='24850149#4') 
+        i=i+1
+
+        traci.person.add(personID="P5", edgeID="616921713#6", pos=15, depart=20, typeID='DEFAULT_PEDTYPE')
+        traci.person.appendStage("P5", stage)
+        traci.person.appendWaitingStage(personID="P5", duration=1000, description='thinking', stopID='24850149#4')
+
     traci.close()
     sys.stdout.flush()
 
-
-
+    
+     
 def get_options():
     optParser = optparse.OptionParser()
     optParser.add_option("--nogui", action="store_true",
