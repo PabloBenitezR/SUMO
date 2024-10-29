46a14bbb606689fed4b3bd6ee3651a382bd4f9f3
PabloBenitezR <141075173+PabloBenitezR@users.noreply.github.com>
2024-10-29 13:27:28 -0600

    Add files via upload

--- CDMXrutas1/runner.py
+++ CDMXrutas1/runner.py
@@ -80,11 +80,18 @@ def newroutes():
 
 def createejbloqueo():
     h = open ('./ejbloqueo.sumocfg','w')
-    h.write("<?xml version='1.0' encoding='UTF-8'?>\n<configuration xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:noNamespaceSchemaLocation='http://sumo.dlr.de/xsd/sumoConfiguration.xsd'>\n<input>\n<net-file value='osm.net.xml.gz'/>\n<route-files value='ejbloqueo.rou.xml'/>\n<additional-files value='closededges.add.xml'/>\n</input>\n</configuration>")
+    h.write("<?xml version='1.0' encoding='UTF-8'?>\n"
+            + "<configuration xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:noNamespaceSchemaLocation='http://sumo.dlr.de/xsd/sumoConfiguration.xsd'>\n"
+            + "<input>\n"
+            + "<net-file value='osm.net.xml.gz'/>\n"
+            + "<route-files value='ejbloqueo.rou.xml'/>\n"
+            + "<additional-files value='closededges.add.xml, caminables.add.xml'/>\n"
+            + "<pedestrian.model value='jupedsim'/>"
+            + "<ignore-route-errors value='true'/>"
+            + "</input>\n"
+            + "</configuration>")
     h.close()
-
-    #agregamos una lista de las calles cerradas para la manifestacion
-    arreglolanes=["616921713#6","616921713#7","616921713#9","616921713#10","616921713#11","616921713#12","616921713#13","616921713#14","616921713#15","1135302411#0","618028624#1","421567701#0","421567701#1","24850149#0","24850149#1","24850149#2","24850149#3","24850149#4"]
+   
 
      
 
@@ -92,21 +99,16 @@ def run():
     step = 0
     #elegimos una semilla aleatoria
     random.seed()
+    primer=arreglolanes[0];
+    print(primer)
+    ultima=arreglolanes[-1];
+    
     while traci.simulation.getMinExpectedNumber() > 0:
+        traci.person.add('person'+str(step), primer, 10, step, 'DEFAULT_PEDTYPE')
+        traci.person.appendWalkingStage('person'+str(step), arreglolanes, 10, -1, -1, '') 
         traci.simulationStep()    
         step += 1
-    primer=arreglolanes[0];
-    ultima=arreglolanes[-1];
-    i=0;
-    while(i<1000):
-        traci.person.add('person'+str(i),edgeID=primer,pos=10, depart=i, typeID='DEFAULT_PEDTYPE')
-        traci.person.appendWalkingStage('person'+str(i), arreglolanes, 10, duration=-1, speed=1, stopID='24850149#4') 
-        i=i+1
-
-        traci.person.add(personID="P5", edgeID="616921713#6", pos=15, depart=20, typeID='DEFAULT_PEDTYPE')
-        traci.person.appendStage("P5", stage)
-        traci.person.appendWaitingStage(personID="P5", duration=1000, description='thinking', stopID='24850149#4')
-
+    
     traci.close()
     sys.stdout.flush()
 
@@ -122,8 +124,10 @@ def get_options():
 
 # this is the main entry point of this script
 if __name__ == "__main__":
-    options = get_options()
+    #agregamos una lista de las calles cerradas para la manifestacion
+    arreglolanes=["616921713#6","616921713#7","616921713#9","616921713#10","616921713#11","616921713#12","616921713#13","616921713#14","616921713#15","1135302411#0","618028624#1","421567701#0","421567701#1","24850149#0","24850149#1","24850149#2","24850149#3","24850149#4"];
 
+    options = get_options()
     closingedges()
     newroutes()
     createejbloqueo()
